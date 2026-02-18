bbe3cfb6ff84c80355d35a5e0b15fa73d6c09a76
Rahaf <rahaf.ogran@gmail.com>
2026-02-18 14:40:54 +0300

    custmoization for register page

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -31,12 +31,17 @@ class RegisteredUserController extends Controller
     {
         $request->validate([
             'name' => ['required', 'string', 'max:255'],
+            'username'=>['required','string','max:30'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
+        // https://ui-avatars.com/api/?name=John+Doe
+
         $user = User::create([
             'name' => $request->name,
+            'image' => 'https://ui-avatars.com/api/?name=' . urlencode($request->name),
+            'username'=>$request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -7,6 +7,11 @@
             <x-input-label for="name" :value="__('Name')" />
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
+        </div >
+          <div class="mt-4">
+            <x-input-label for="username" :value="__('UserName')" />
+            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
